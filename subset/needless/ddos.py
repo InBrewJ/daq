3778851531bac67957fad23aaafacf0eb2cf3be6
InBrewJ <jason.brewer@arupiot.com>
2020-01-31 18:12:00 +0000

    test_needless module now works fine with two test results

--- subset/needless/ddos.py
+++ subset/needless/ddos.py
@@ -6,18 +6,25 @@ import random
 import argparse
 from time import sleep
 
-REPORT_OUT = '/tmp/local.report.txt'
+RESULTS_OUT = '/tmp/result_lines.txt'
 
 # Options to control the flow of things
 parser = argparse.ArgumentParser()
 parser.add_argument("--port", "-p", help="Starting port to DDoS attack")
 parser.add_argument("--address", "-a", help="IP address to DDoS attack")
 parser.add_argument("--skip", "-s", help="Skip the test for no apparent reasons", action="store_true")
+parser.add_argument("--nothing", "-n", help="Do nothing", action="store_true")
 args = parser.parse_args()
 
+if args.nothing:
+    with open(RESULTS_OUT, 'a') as f:
+        print("This test did nothing")
+        f.write("RESULT pass needless.nothing Nothing happened\n")
+    exit()
+
 if args.skip:
-    with open(REPORT_OUT, 'w') as f:
-        f.write("RESULT info needless.script.kiddie Someone skipped the test!")
+    with open(RESULTS_OUT, 'w') as f:
+        f.write("RESULT info needless.script.kiddie Someone skipped the test!\n")
     exit()
 
 ##############
@@ -47,11 +54,11 @@ while timer > 0:
     except Exception as e:
         print("Could not DDoS, test failed!")
         print("Why: ", e)
-        with open(REPORT_OUT, 'w') as f:
+        with open(RESULTS_OUT, 'w') as f:
             print("failed! writing report...")
-            f.write("RESULT fail needless.script.kiddie Could not DDoS because reasons")
+            f.write("RESULT fail needless.script.kiddie Could not DDoS because reasons\n")
         exit()
 
-with open(REPORT_OUT, 'w') as f:
+with open(RESULTS_OUT, 'w') as f:
     print("PASSED writing report...")
-    f.write("RESULT pass needless.script.kiddie The server is now down. hooray")
+    f.write("RESULT pass needless.script.kiddie The server is now down. hooray\n")
