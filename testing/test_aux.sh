892b404f41d6dea27ca6fb89253cffd92668d688
InBrewJ <jason.brewer@arupiot.com>
2020-02-01 22:10:02 +0000

    Amended integration test output logs, fingers crossed

--- testing/test_aux.sh
+++ testing/test_aux.sh
@@ -106,6 +106,7 @@ capture_aux_test_results macoui all
 capture_aux_test_results tls all
 capture_aux_test_results password all
 capture_aux_test_results discover all
+capture_aux_test_results needless all
 
 # Capture peripheral logs
 more inst/run-port-*/scans/dhcp_triggers.txt | cat
@@ -123,6 +124,7 @@ more inst/run-port-*/nodes/tls*/activate.log | cat
 more inst/run-port-*/nodes/password*/activate.log | cat
 more inst/run-port-*/nodes/discover*/activate.log | cat
 more inst/run-port-*/nodes/bacext*/activate.log | cat
+more inst/run-port-*/nodes/needless*/activate.log | cat
 ls inst/fail_fail01/ | tee -a $TEST_RESULTS
 
 # Add the port-01 and port-02 module config into the file
